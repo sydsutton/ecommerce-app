b0fe461de193d1790d33992f2aefbff6dc55c662
Andrew Sutton <andrewsydsutton@gmail.com>
2022-02-03 12:32:57 -0600

    sign up component progress

--- src/components/CategoryComponent.js
+++ src/components/CategoryComponent.js
@@ -7,7 +7,7 @@ import {Pagination, Stack} from "@mui/material"
 import {productsData} from "../data"
 
 const CategoryComponent = () => {
-
+    const {category} = useParams()
     const [page, setPage] = useState(1)
     //scrolling up to the top of the page every time the page changes
     useEffect(() => {
@@ -18,7 +18,6 @@ const CategoryComponent = () => {
         }
     }, [page])
 
-    const {category} = useParams()
     //Added this so that I can reuse the Category component for the brands. 
     //Then used conditional rendering in the filtering of the data
     const listOfCategories = ["jeans", "hoodie", "jackets", "suits", "tshirt"]
@@ -32,7 +31,7 @@ const CategoryComponent = () => {
     const products = filteredData.slice(page === firstPage ? 0 : (page - 1) * 10, page === lastPage ? filteredData.length - (numberOfPages - 1) * 10 : page * 10)
 
     return (
-        <div>
+        <div className="pb-5">
             <div className="jumbotron">
                 <CarouselComponent />
             </div>
@@ -40,27 +39,33 @@ const CategoryComponent = () => {
             {/* Did this to make the category plural for the title */}
             <h2 className="category-title">{listOfCategories.includes(category) && category[category.length - 1] !== "s" ? category + "s" : category }</h2>
             <div className="container">
-                {products.map((product, index) => {
-                    // Had to put this in because some of the discounts were larger than the
-                    //original prices, causing a negative price on a lot of items
-                    const salePrice = product.originPrice > product.discount ? (product.originPrice - product.discount) : product.discount - product.originPrice
+                <div className="row justify-content-center">
+                    {products.map((product, index) => {
+                        // Had to put this in because some of the discounts were larger than the
+                        //original prices, causing a negative price on a lot of items
+                        const salePrice = product.originPrice > product.discount ? (product.originPrice - product.discount) : product.discount - product.originPrice
 
-                    return (
-                        <div className="product-link m-2" key={index}>
-                           <ItemCardComponent salePrice={salePrice} product={product} index={index} category={category} />
-                        </div>
-                    )
-                })}
+                        return (
+                            <div className="product-link m-2" key={index}>
+                            <ItemCardComponent salePrice={salePrice} product={product} index={index} category={category} />
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
-            <Stack spacing={2}>
-                <Pagination 
-                    color="primary" 
-                    count={numberOfPages} 
-                    className="my-3 mx-auto"
-                    page={page}
-                    onChange={(e, value) => setPage(value)}
-                />
-            </Stack>
+            {numberOfPages === 1 ? 
+                null 
+            : 
+                <Stack spacing={2}>
+                    <Pagination 
+                        color="primary"
+                        count={numberOfPages} 
+                        className="my-3 mx-auto"
+                        page={page}
+                        onChange={(e, value) => setPage(value)}
+                    />
+                </Stack>
+            }
         </div>
     );
 };
--- src/components/SignInComponent.js
+++ src/components/SignInComponent.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import {
+    Input,
+    Button,
+    FormControl,
+    InputLabel,
+    FormGroup
+} from "@mui/material"
 
 const SignInComponent = () => {
     return (
-        <div>
-            <h1>Sign In</h1>
+        <div className="container">
+            <div className="row">
+                <h1>Sign In</h1>
+                <form className="w-50 mx-auto p-3 sign-in-form" style={{maxWidth: "400px", height: "200px"}}>
+                    <FormGroup>
+                        <FormControl className="mb-3">
+                            <InputLabel htmlFor="email">Email address</InputLabel>                        
+                            <Input id="email" type="text" required/>
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel htmlFor="password">Password</InputLabel>     
+                            <Input type="password" id="password" required/>
+                        </FormControl>
+                    </FormGroup>
+                    <Button type="submit" variant="outlined" className="mt-3">Log In</Button>
+                </form>
+            </div>
         </div>
     );
 };
