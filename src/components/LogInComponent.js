b5b931eb2db98ff3a7b927df2bb8b0d68c5550e2
Andrew Sutton <andrewsydsutton@gmail.com>
2022-02-07 21:02:10 -0600

    firebase is working properly

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -6,7 +6,9 @@ import {
     InputLabel,
     FormGroup,
     Modal,
-    Box
+    Box,
+    Snackbar,
+    Alert
 } from "@mui/material"
 
 import SignUp from "./SignUpComponent"
@@ -18,62 +20,91 @@ import {Context} from "../Context"
 const LogInComponent = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
     const [isChildModalOpen, setIsChildModalOpen] = useState(false)
+    const [snackbarOpen, setSnackbarOpen] = useState(false)
 
     const {isModalOpen, setIsModalOpen, logIn, setIsDrawerOpen, isDrawerOpen} = useContext(Context)
 
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        setErrorMessage("")
+        try {
+            await logIn(email, password)
+            .then(() => {
+                setSnackbarOpen(true)
+                setIsModalOpen(false)
+            })
+            .catch((error) => setErrorMessage(error.message))
+        } catch {
+            setErrorMessage("Sorry, something went wrong")
+        }
+    }
+
     return (
-        <Modal 
-            open={isModalOpen} 
-            onClose={() => setIsModalOpen(false)} 
-        >
-            <Box className="signin-modal">
-                <form className="p-4 sign-in-form pt-2" onSubmit={() => logIn(email, password)}>
-                    <h4 className="mb-3">Log In</h4>
-                    <hr className="mb-3" />
-                    <FormGroup>
-                        <FormControl className="mb-3">
-                            <InputLabel htmlFor="email">Email address</InputLabel>                        
-                            <Input 
-                                autoFocus={true}
-                                id="email" 
-                                type="text" 
-                                value={email} 
-                                onChange={(e) => setEmail(e.target.value)} 
-                                required
-                            />
-                        </FormControl>
-                        <FormControl className="mb-3">
-                            <InputLabel htmlFor="password" shrink={password !== "" ? true : false}>Password</InputLabel>     
-                            <Input 
-                                type="password" 
-                                id="password" 
-                                value={password}
-                                onChange={(e) => setPassword(e.target.password)}
-                                required/>
-                        </FormControl>
-                    </FormGroup>
-                    <Button 
-                        type="submit" 
-                        variant="outlined" 
-                        className="mt-3"
-                    >
-                        Log In
-                    </Button>
-                    <div className="d-flex flex-row mt-3 justify-content-evenly">
-                        <p>Don't have an account?</p>
-                        <Button
-                            onClick={() => {
-                                setIsChildModalOpen(true)
-                            }}
+        <React.Fragment>
+            <Modal 
+                open={isModalOpen} 
+                onClose={() => setIsModalOpen(false)} 
+            >
+                <Box className="signin-modal">
+                    <form className="p-4 sign-in-form pt-2" onSubmit={handleSubmit}>
+                        <h4 className="mb-3">Log In</h4>
+                        <hr className="mb-3" />
+                        <div>{errorMessage ? errorMessage : null}</div>
+                        <FormGroup>
+                            <FormControl className="mb-3">
+                                <InputLabel htmlFor="email">Email address</InputLabel>                        
+                                <Input 
+                                    autoFocus={true}
+                                    id="email" 
+                                    type="text" 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    required
+                                />
+                            </FormControl>
+                            <FormControl className="mb-3">
+                                <InputLabel htmlFor="password" shrink={password !== "" ? true : false}>Password</InputLabel>     
+                                <Input 
+                                    type="password" 
+                                    id="password" 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required/>
+                            </FormControl>
+                        </FormGroup>
+                        <Button 
+                            type="submit" 
+                            variant="outlined" 
+                            className="mt-3"
                         >
-                            Sign Up
+                            Log In
                         </Button>
-                    </div>
-                </form>
-                <SignUp isChildModalOpen={isChildModalOpen} setIsChildModalOpen={setIsChildModalOpen} />
-            </Box>
-        </Modal>
+                        <div className="d-flex flex-row mt-3 justify-content-evenly">
+                            <p>Don't have an account?</p>
+                            <Button
+                                onClick={() => {
+                                    setIsChildModalOpen(true)
+                                }}
+                            >
+                                Sign Up
+                            </Button>
+                        </div>
+                    </form>
+                    <SignUp isChildModalOpen={isChildModalOpen} setIsChildModalOpen={setIsChildModalOpen} />
+                </Box>
+            </Modal>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                onClose={() => setSnackbarOpen(false)}
+            >
+                <Alert severity="success">
+                    {`You've successfully logged in!`}
+                </Alert>
+            </Snackbar>
+        </React.Fragment>
     );
 };
 
--- src/components/SignUpComponent.js
+++ src/components/SignUpComponent.js
@@ -8,7 +8,9 @@ import {
     FormControl,
     Button,
     Modal,
-    Box
+    Box,
+    Snackbar,
+    Alert
 } from "@mui/material"
 
 const SignUpComponent = ({isChildModalOpen, setIsChildModalOpen}) => {
@@ -17,67 +19,97 @@ const SignUpComponent = ({isChildModalOpen, setIsChildModalOpen}) => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
+    const [errorMessage, setErrorMessage] = useState("")
+    const [snackbarOpen, setSnackbarOpen] = useState(false)
+
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        setErrorMessage("")
+        try {
+            await signUp(email, password)
+            .then(() => {
+                setSnackbarOpen(true)
+                setIsChildModalOpen(false)
+            })
+            .catch((error) => setErrorMessage(error.message))
+        } catch {
+            setErrorMessage("Sorry, something went wrong")
+        }
+    }
 
     return (
-        <Modal 
-            open={isChildModalOpen} 
-            onClose={() => setIsChildModalOpen(false)}
-        >
-            <Box className="child-modal">
-                <form className="p-4 sign-up-form pt-2" onSubmit={() => signUp(email, password)}>
-                    <h4 className="mb-3">Sign Up</h4>
-                    <hr />
-                    <FormGroup>
-                        <FormControl className="mb-3">
-                            <InputLabel htmlFor="email">Email address</InputLabel>                        
-                            <Input 
-                                id="email" 
-                                autoFocus={true}
-                                type="text" 
-                                value={email} 
-                                onChange={(e) => setEmail(e.target.value)} 
-                                required
-                            />
-                        </FormControl>
-                        <FormControl className="mb-3">
-                            <InputLabel htmlFor="password" shrink={password !== "" ? true : false}>Password</InputLabel>     
-                            <Input 
-                                type="password" 
-                                id="text" 
-                                value={password}
-                                onChange={(e) => setPassword(e.target.password)}
-                                required/>
-                        </FormControl>
-                        <FormControl>
-                            <InputLabel htmlFor="confirmPassword" shrink={confirmPassword !== "" ? true : false}>Confirm password</InputLabel>     
-                            <Input 
-                                type="password" 
-                                id="confirmPassword" 
-                                value={confirmPassword}
-                                onChange={(e) => setConfirmPassword(e.target.password)}
-                                required/>
-                        </FormControl>
-                    </FormGroup>
-                    <Button 
-                        type="submit" 
-                        variant="outlined" 
-                        className="mt-3"
-                    >
-                        Sign Up
-                    </Button>
-                    <div className="d-flex flex-row mt-3 justify-content-evenly">
-                        <p>Already have an accout?</p>
-                        <Button
-                            onClick={() => {
-                                setIsChildModalOpen(false)
-                            }}
+        <React.Fragment>
+            <Modal 
+                open={isChildModalOpen} 
+                onClose={() => setIsChildModalOpen(false)}
+            >
+                <Box className="child-modal">
+                    <form className="p-4 sign-up-form pt-2" onSubmit={handleSubmit}>
+                        <h4 className="mb-3">Sign Up</h4>
+                        <hr />
+                        <FormGroup>
+                            <div>{errorMessage ? errorMessage : null}</div>
+                            <FormControl className="mb-3">
+                                <InputLabel htmlFor="email">Email address</InputLabel>                        
+                                <Input 
+                                    id="email" 
+                                    autoFocus={true}
+                                    type="text" 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    required
+                                />
+                            </FormControl>
+                            <FormControl className="mb-3">
+                                <InputLabel htmlFor="password" shrink={password !== "" ? true : false}>Password</InputLabel>     
+                                <Input 
+                                    type="password" 
+                                    id="text" 
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required/>
+                            </FormControl>
+                            <FormControl>
+                                <InputLabel htmlFor="confirmPassword" shrink={confirmPassword !== "" ? true : false}>Confirm password</InputLabel>     
+                                <Input 
+                                    type="password" 
+                                    id="confirmPassword" 
+                                    value={confirmPassword}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                    required/>
+                            </FormControl>
+                        </FormGroup>
+                        <Button 
+                            type="submit" 
+                            variant="outlined" 
+                            className="mt-3"
                         >
-                            Log In
+                            Sign Up
                         </Button>
-                    </div>
-                </form>
-            </Box>
-        </Modal>
+                        <div className="d-flex flex-row mt-3 justify-content-evenly">
+                            <p>Already have an accout?</p>
+                            <Button
+                                onClick={() => {
+                                    setIsChildModalOpen(false)
+                                }}
+                            >
+                                Log In
+                            </Button>
+                        </div>
+                    </form>
+                </Box>
+            </Modal>
+            <Snackbar
+                open={snackbarOpen}
+                autoHideDuration={3000}
+                onClose={() => setSnackbarOpen(false)}
+            >
+                <Alert severity="success">
+                    {`You've successfully signed up!`}
+                </Alert>
+            </Snackbar>
+        </React.Fragment>
     );
 };
 
